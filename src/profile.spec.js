2aeb5cd2045bee2701c4319f1d80eedf8191331b
cburk <cjb6@rice.edu>
2017-04-03 14:40:14 -0500

    full put and get headline test

--- src/profile.spec.js
+++ src/profile.spec.js
@@ -23,7 +23,19 @@ describe('Validate Profile functionality', () => {
 			expect(JSON.parse(body).username).to.eql(testUsername)
 			expect(JSON.parse(body).headline).to.eql(newHeadline.headline)
                 }).catch(done)
-		.then(done)
+		.then(body => {
+			// Verify headline was updated with a get request
+			fetch(url("/headlines"))
+			.then(res => {
+	                        expect(res.status).to.eql(200)
+	                        return res.text()
+			})
+			.then(body => {
+				const relevantHeadlines = JSON.parse(body).headlines.filter((x)=>{return x.username == testUsername})
+				console.log("Mostly through, did we get headlines? ", relevantHeadlines)
+				expect(relevantHeadlines[0].headline).to.eql(newHeadline.headline)
+			}).then(done)
+		})
         }, 200)
 
 });
