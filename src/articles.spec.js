115e283e84168b3ab5c76fc92b9542d7b0ef8677
cburk <cjb6@rice.edu>
2017-04-03 21:42:45 -0500

    fixed article tests, actually passing and consistently now

--- src/articles.spec.js
+++ src/articles.spec.js
@@ -7,7 +7,6 @@ const fetch = require('isomorphic-fetch')
 const url = path => `http://localhost:3000${path}`
 
 describe('Validate Article functionality', () => {
-	/*
 	it('should give me three or more articles', (done) => {
 		// IMPLEMENT ME
 		fetch(url("/articles"))
@@ -16,7 +15,7 @@ describe('Validate Article functionality', () => {
 			return res.text()
 		})
 		.then(body => {
-			expect(JSON.parse(body).length).above(2)
+			expect(JSON.parse(body).articles.length).above(2)
 		})
 		.then(done)
 		.catch(done)
@@ -34,83 +33,79 @@ describe('Validate Article functionality', () => {
 			headers: { 'Content-Type': 'application/json' },
 			body: JSON.stringify(thisBody)
 		})
-                .then(res => {
-                        expect(res.status).to.eql(200)
-                        return res.text()
-                })
-                .then(body => {
-                        expect(JSON.parse(body).author).to.eql(thisBody.author)
-                        expect(JSON.parse(body).text).to.eql(thisBody.text)
-			firstId = JSON.parse(body).id
-                }).catch(done)
-
-		//Post 2
-		// add a second article
-		// verify the article id increases by one
-		// verify the second artice has the correct content
-                fetch(url("/article"), {
-                        method: "POST",
-                        headers: { 'Content-Type': 'application/json' },
-                        body: JSON.stringify(thisBody2)
-                })
-                .then(res => {
-                        expect(res.status).to.eql(200)
-                        return res.text()
-                })
-                .then(body => {
-                        expect(JSON.parse(body).author).to.eql(thisBody2.author)
-                        expect(JSON.parse(body).text).to.eql(thisBody2.text)
-                        expect(JSON.parse(body).id).to.eql(firstId + 1)
-		})
-                .then(done)
-                .catch(done)
+        .then(res => {
+                expect(res.status).to.eql(200)
+                return res.text()
+        })
+        .then(body => {
+                console.log("For this part of test: ", JSON.parse(body))
+                const arts = JSON.parse(body).articles
+                expect(arts[arts.length - 1].text).to.eql(thisBody.text)
+                firstId = arts[arts.length - 1].id
+        }).then(body => {
+            //Post 2
+            // add a second article
+            // verify the article id increases by one
+            // verify the second artice has the correct content
+            fetch(url("/article"), {
+                    method: "POST",
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(thisBody2)
+            })
+            .then(res => {
+                    expect(res.status).to.eql(200)
+                    return res.text()
+            })
+            .then(body => {
+                    const secondArts = JSON.parse(body).articles
+                    expect(secondArts[secondArts.length - 1].text).to.eql(thisBody2.text)
+                    expect(secondArts[secondArts.length - 1].id).to.eql(firstId + 1)
+            })
+        })
+        .then(done)
+        .catch(done)
 
  	}, 200)
 
 	it('should return an article with a specified id', (done) => {
 		let anId
-                fetch(url("/articles"))
-                .then(res => {
-                        expect(res.status).to.eql(200)
-                        return res.text()
-                })
-                .then(body => {
-                        anId = 1
-                })
-                .catch(done)
-
-		//Try to get the actual id
-                fetch(url("/articles/1"))
-                .then(res => {
-			//console.log("Tried for url: ", url("/articles/" + anId))
-                        expect(res.status).to.eql(200)
-                        return res.text()
-                })
-                .then(body => {
-			console.log("test 3 body: ", body)
-			expect(JSON.parse(body)[0].id).to.eql(anId)
-                })
-                .then(done)
-                .catch(done)
+        fetch(url("/articles"))
+        .then(res => {
+                expect(res.status).to.eql(200)
+                return res.text()
+        })
+        .then(body => {
+                anId = JSON.parse(body).articles[0]._id
+                console.log("Found id: ", anId)
+        }).then(body => {
+            //Try to get the actual id
+            fetch(url("/articles", anId))
+            .then(res => {
+			     //console.log("Tried for url: ", url("/articles/" + anId))
+                expect(res.status).to.eql(200)
+                return res.text()
+            })
+            .then(body => {
+                expect(JSON.parse(body).articles[0]._id).to.eql(anId)
+            })
+            .then(done)
+            .catch(done)
+        })
 	}, 200)
 
 	it('should return nothing for an invalid id', (done) => {
 		// call GET /articles/id where id is not a valid article id, perhaps 0
 		// confirm that you get no results
 		fetch(url("/articles/10000"))
-                .then(res => {
-                        expect(res.status).to.eql(200)
-                        return res.text()
-                })
-                .then(body => {
+        .then(res => {
+                expect(res.status).to.eql(200)
+                return res.text()
+        })
+        .then(body => {
 			console.log(body)
-			expect(JSON.parse(body).length).to.eql(0)
+			expect(JSON.parse(body).articles.length).to.eql(0)
                 })
 		.then(done)
-                .catch(done)
-
-
-		//done(new Error('Not implemented'))
+        .catch(done)
 	}, 200)
-	*/
 });
