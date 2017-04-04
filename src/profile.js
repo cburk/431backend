1ca653dd6dec50c1099cfd37c11bb58b4c7a549d
cburk <cjb6@rice.edu>
2017-04-03 19:14:33 -0500

    dob

--- src/profile.js
+++ src/profile.js
@@ -3,6 +3,7 @@ const loggedInProfile = {
 	headline: 'Logged in headline',
 	email: 'logged@in.com',
 	zipcode: 12345,
+	dob: 687610808426.5197,
 	avatar: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4e/DWLeebron.jpg/220px-DWLeebron.jpg'
 }
 
@@ -88,6 +89,10 @@ const avatarF = (req, res) => {
         res.send(newAvatar)
 }
 
+const dob = (req, res) => {
+	res.send({username: loggedInProfile.username, dob: loggedInProfile.dob})
+}
+
 module.exports = app => {
 	app.get('/headlines/:user?', headlines),
 	app.put('/headline', headline),
@@ -96,5 +101,6 @@ module.exports = app => {
 	app.get('/zipcode/:user?', getZip),
 	app.put('/zipcode', putZip),
 	app.get('/avatars/:user?', avatarsF),
-	app.put('/avatar', avatarF)
+	app.put('/avatar', avatarF),
+	app.get('/dob', dob)
 }
