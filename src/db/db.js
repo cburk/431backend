3d6565cacd2fe5af4360a1d604d36c0a057698e2
cburk <cjb6@rice.edu>
2017-05-01 13:53:21 -0500

    new db so we don't mess w/ 7

--- src/db/db.js
+++ src/db/db.js
@@ -1,6 +1,11 @@
 var mongoose = require('mongoose')
 
+//DB From 7
+/*
 var url = 'mongodb://heroku_lkfs7t5w:c8rb4t8j4r6j2hehh27sn909bi@ds161630.mlab.com:61630/heroku_lkfs7t5w'
+*/
+var url = 'mongodb://heroku_zr1zljv2:ev7n5njvs899p1i53u2cnpv7te@ds127801.mlab.com:27801/heroku_zr1zljv2'
+
 
 mongoose.connect(url)
 
